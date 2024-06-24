497e8669cbb62a221bf6cf759893e7b5725b3a77
SamuelHollis <150815327+SamuelHollis@users.noreply.github.com>
2024-06-24 18:06:59 +0000

    ej3 compl

--- exercises/03-response-body/app.py
+++ exercises/03-response-body/app.py
@@ -1,3 +1,10 @@
 import requests
 
-url = "https://assets.breatheco.de/apis/fake/sample/random-status.php"
+response = requests.get("https://assets.breatheco.de/apis/fake/sample/random-status.php")
+
+
+
+if response.status_code == 200:
+    print(response.text)
+else:
+    print("Something went wrong")
