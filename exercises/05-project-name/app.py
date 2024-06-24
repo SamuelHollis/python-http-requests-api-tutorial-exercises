efe09155fe8e44647b434c0fcf5f34867a3f8584
SamuelHollis <150815327+SamuelHollis@users.noreply.github.com>
2024-06-24 18:27:07 +0000

    ej5 compl

--- exercises/05-project-name/app.py
+++ exercises/05-project-name/app.py
@@ -1,3 +1,11 @@
 import requests
 
-# Your code here
+response = requests.get("https://assets.breatheco.de/apis/fake/sample/project1.php")
+
+response_body = response.json()
+if response.status_code == 200:
+    name_response = response_body['name']
+    print(name_response)
+    
+else:
+    print('something went wrong')
