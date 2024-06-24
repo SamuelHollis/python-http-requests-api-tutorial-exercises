9ca54809fc28c24f174966d3600744dc1e05c1d7
SamuelHollis <150815327+SamuelHollis@users.noreply.github.com>
2024-06-24 18:18:03 +0000

    ej4 compl

--- exercises/04-response-body-json/app.py
+++ exercises/04-response-body-json/app.py
@@ -2,3 +2,16 @@ import requests
 
 response = requests.get("https://assets.breatheco.de/apis/fake/sample/time.php")
 print(response.text)
+
+date = response.json()
+
+hour = date['hours']
+minutes = date['minutes']
+seconds = date['seconds']
+
+if response.status_code == 200:
+    print(f"Current time: {hour}hrs {minutes}min {seconds}sec")
+    
+else:
+    print('something went wrong')
+
